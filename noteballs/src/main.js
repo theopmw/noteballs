97f37879c6e844799e9bf0baad425a56983d2904
theopmw <theowrightdev@gmail.com>
2023-10-13 08:31:56 +0100

    initial vue router setup

--- noteballs/src/main.js
+++ noteballs/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [],
+});
+createApp(App).use(router).mount('#app');
