8570269515f8f04f55b238a993cdbb4138e0cd5b
theopmw <theowrightdev@gmail.com>
2023-10-19 07:37:38 +0100

    add note cards

--- noteballs/src/views/ViewNotes.vue
+++ noteballs/src/views/ViewNotes.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="notes">
-    <h1>Notes</h1>
+    <div class="card mb-4" v-for="i in 3">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+          facilis nam repellat cumque autem est laborum, modi officia culpa odio
+          dignissimos sit ad repudiandae quia? Iusto officia illo rem!
+          Assumenda!
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
